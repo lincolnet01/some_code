35f107b56a5994870a08b4bfac50f1b6b389a4ed
Lincoln <kamanoabra2014@outlook.com>
2023-05-17 15:39:48 +0000

    Another test

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added as completed"
-echo "This is another line"
-echo "This is the added line 1"
+echo "This was git learning with jenkins"
