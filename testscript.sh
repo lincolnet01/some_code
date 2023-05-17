d56e37ab48a911c0788ecb2b7ab8ce4f8a62cf66
Lincoln <kamanoabra2014@outlook.com>
2023-05-17 16:25:42 +0000

    Another test 1

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "Step 1 and 2 and 3 and 4 startring..."
+echo "Step 1 and 2 and 3 and 4 and 5 tartring..."
